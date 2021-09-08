eace44a67af51784442e59d46d3206d4fc02f7f5
Argha Nilanjon Nondi <pcic095@gmail.com>
2021-09-08 15:23:41 +0600

    Fixed the redirect issue

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -1,34 +1,16 @@
-import React, { Component, Fragment } from 'react'
-import LoadingBar from './LoadingBar'
-import {
-  Redirect
-} from "react-router-dom"
-export default class Logout extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            loginRedirect:false
-        }
-    }
+import React, {Fragment, useEffect } from "react";
+import LoadingBar from "./LoadingBar";
+import { useHistory} from "react-router-dom";
 
-    componentDidMount=()=>{
-        localStorage.clear();
-    }
-    
-    render() {
-
-
-        setTimeout(()=>{
-           this.setState({
-               loginRedirect:true
-           })
-        },2000);
-
-        return(<Fragment>
-            <LoadingBar className="mt-4" style={{margin:"0 auto"}}></LoadingBar>
-            {
-              this.state.loginRedirect?<Redirect to="/login/"></Redirect>:""
-            }
-        </Fragment>)
-    }
+export default function Logout() {
+    let history=useHistory();
+  useEffect(() => {
+    localStorage.clear();
+    history.push("/login/")
+  });
+  return (
+    <Fragment>
+      <LoadingBar className="mt-4" style={{ margin: "0 auto" }}></LoadingBar>
+    </Fragment>
+  );
 }
--- src/components/User.js
+++ src/components/User.js
@@ -1,55 +1,59 @@
-import React, { Component,Fragment,useState } from 'react'
+import React, { Component, Fragment, useState, useEffect } from "react";
 import {
   BrowserRouter as Router,
-  Switch,
+  Redirect,
   Route,
   useRouteMatch,
-  Redirect
-} from "react-router-dom"
-import Admin from './users/Admin'
-import Teacher from './users/Teacher'
-import Student from './users/Student'
-import Not_found from './404'
-import LoadingBar from './LoadingBar'
-import axios from 'axios'
+} from "react-router-dom";
+import Admin from "./users/Admin";
+import Teacher from "./users/Teacher";
+import Student from "./users/Student";
+import Not_found from "./404";
+import LoadingBar from "./LoadingBar";
+import axios from "axios";
 
-
-  
-
-function getUserRoutes(user,path){
+function getUserRoutes(user, path) {
   let content;
-
-  if(user==="admin"){
-     content=(
-       <Router>
-         <Route path={`${path}/admin`}>
-           <Admin></Admin>
-         </Route>
-       </Router>
-     );
+  if (user === "admin") {
+    content = (
+      <Fragment>
+        <Router>
+          <Route exact path={`${path}/admin`}>
+            <Admin></Admin>
+          </Route>
+          <Redirect to={`${path}/admin`}></Redirect>
+        </Router>
+      </Fragment>
+    );
   }
 
-  if(user==="teacher"){
-     content=(
-      <Router>
-      <Route path={`${path}/teacher`}>
-        <Teacher></Teacher>
-      </Route>
-    </Router>
-     );
+  if (user === "teacher") {
+    content = (
+      <Fragment>
+        <Router>
+          <Route exact path={`${path}/teacher`}>
+            <Teacher></Teacher>
+          </Route>
+          <Redirect to={`${path}/teacher`}></Redirect>
+        </Router>
+      </Fragment>
+    );
   }
 
-  if(user==="student"){
-     content=(
-      <Router>
-      <Route path={`${path}/student`}>
-        <Student></Student>
-      </Route>
-    </Router>
-     );
+  if (user === "student") {
+    content = (
+      <Fragment>
+        <Router>
+          <Route exact path={`${path}/student`}>
+            <Student></Student>
+          </Route>
+          <Redirect to={`${path}/student`}></Redirect>
+        </Router>
+      </Fragment>
+    );
   }
 
-  if(user==="unknown"){
+  if (user === "unknown") {
     content = (
       <div className="container my-4">
         <LoadingBar></LoadingBar>
@@ -57,53 +61,45 @@ function getUserRoutes(user,path){
     );
   }
 
-  if(user==="unauthorize"){
-    content=(
-    <Redirect to="/login"></Redirect>
-    );
+  if (user === "unauthorize") {
+    content = <Redirect to="/login"></Redirect>;
   }
 
   return content;
-
 }
 
-
-export default function User(){
-
+export default function User() {
   let { path } = useRouteMatch();
 
-  let [isAuth,setIsAuth]=useState("unknown");
+  useEffect(() => {
+    console.log(path);
+  });
 
-  if(localStorage.token===undefined || localStorage.usertype===undefined){
-    return (
-     <Redirect to="/login"></Redirect>
-    );
+  let [isAuth, setIsAuth] = useState("unknown");
+
+  if (localStorage.token === undefined || localStorage.usertype === undefined) {
+    return <Redirect to="/login"></Redirect>;
   }
 
   axios({
-    method: 'post',
+    method: "post",
     url: `http://${document.location.hostname}/student-management-system/validation.php`,
     data: {
-        "token":localStorage.getItem("token"),
-        "usertype":localStorage.getItem("usertype")
-      }
+      token: localStorage.getItem("token"),
+      usertype: localStorage.getItem("usertype"),
+    },
   })
-  .then((response)=>{
-      if(response.data.code==="3003"){
+    .then((response) => {
+      if (response.data.code === "3003") {
         setIsAuth("unauthorize");
       }
-      if(response.data.code==="2300"){
-        setIsAuth(localStorage.usertype)
+      if (response.data.code === "2300") {
+        setIsAuth(localStorage.usertype);
       }
-  })
-  .catch((err)=>{
+    })
+    .catch((err) => {
       return err;
-  })
+    });
 
-  return (<Fragment>
-      {
-        
-          getUserRoutes(isAuth,path)
-      }
-    </Fragment>)
-    }
+  return <Fragment>{getUserRoutes(isAuth, path)}</Fragment>;
+}
